85910a9c53d2d041d731237e73ee198a6d7ae5d4
Grant Braught <braughtg@gmail.com>
2023-12-31 17:41:03 +0000

    Handles the pull, create and start.

--- startup.bash
+++ startup.bash
@@ -1,21 +1,43 @@
-#!/bin/bash
+IMAGE_PULLED=$(docker image ls | tail -n +2)
+if [ -z "$IMAGE_PULLED" ];
+then
+    echo "Pulling VNC/noVNC image..."
+    docker pull braughtg/vnc-novnc-base:1.2.1
+    echo "Pulled."
+    echo ""
+fi
 
-echo "Waiting for VNC/noVNC servers to start..."
-gp ports await 5901 > /dev/null
-gp ports await 6901 > /dev/null
-echo "Started."
-echo ""
+CONTAINER_EXISTS=$(docker ps -a | grep "vnc")
+if [ -z "$CONTAINER_EXISTS" ];
+then
+    echo "Creating VNC/noVNC container..."
+    docker create --name vnc --publish 5901:5901 --publish 6901:6901 braughtg/vnc-novnc-base:1.2.1
+    echo "Created."
+    echo ""
+fi
 
-echo "Connect to the noVNC server with your browser at:"
-gp url 6901
-echo ""
-echo "OR"
-echo ""
+CONTAINER_RUNNING=$(docker ps | grep "vnc")
+if [ -z "$CONTAINER_RUNNING" ];
+then
+    echo "Starting VNC/noVNC container"
+    docker start vnc
+    gp ports await 5901 > /dev/null
+    gp ports await 6901 > /dev/null
+    echo "Started."
+    echo ""
+    echo ""
 
-echo "Do the following on our machine to connect via VNC:"
-SSH_URL=$(gp ssh | cut -f2 -d' ')
-echo "  Run the command:"
-echo "    ssh -L 5901:localhost:5901 -o UserKnownHostsFile=/dev/null $SSH_URL"
-echo "  Connect your VNC Client to:"
-echo "    localhost:5901"
-echo ""
+    echo "Connect to the noVNC server with your browser at:"
+    gp url 6901
+    echo ""
+    echo "OR"
+    echo ""
+
+    echo "Do the following on our machine to connect via VNC:"
+    SSH_URL=$(gp ssh | cut -f2 -d' ')
+    echo "  Run the command:"
+    echo "    ssh -L 5901:localhost:5901 $SSH_URL"
+    echo "  Connect your VNC Client to:"
+    echo "    localhost:5901"
+    echo ""
+fi
