614f8179670605dd85a2885e1c84521e2d769953
Grant Braught <braughtg@gmail.com>
2024-01-02 15:33:06 +0000

    Update script output to say KitClient.

--- startup.bash
+++ startup.bash
@@ -1,7 +1,7 @@
 IMAGE_PULLED=$(docker image ls | tail -n +2)
 if [ -z "$IMAGE_PULLED" ];
 then
-    echo "Pulling VNC/noVNC image..."
+    echo "Pulling KitClient image..."
     #docker pull braughtg/vnc-novnc-base:1.2.1
     docker pull registry.gitlab.com/hfossedu/kits/kitclient:latest
     echo "Pulled."
@@ -11,7 +11,7 @@ fi
 CONTAINER_EXISTS=$(docker ps -a | grep "vnc")
 if [ -z "$CONTAINER_EXISTS" ];
 then
-    echo "Creating VNC/noVNC container..."
+    echo "Creating KitClient container..."
     #docker create --name vnc --publish 5901:5901 --publish 6901:6901 braughtg/vnc-novnc-base:1.2.1
     docker create --name KitClient -p 6901:6901 -p 5901:5901 registry.gitlab.com/hfossedu/kits/kitclient:latest
     echo "Created."
@@ -21,7 +21,7 @@ fi
 CONTAINER_RUNNING=$(docker ps | grep "vnc")
 if [ -z "$CONTAINER_RUNNING" ];
 then
-    echo "Starting VNC/noVNC container"
+    echo "Starting KitClient container"
     #docker start vnc
     docker start KitClient
     gp ports await 5901 > /dev/null
@@ -30,13 +30,13 @@ then
     echo ""
     echo ""
 
-    echo "Connect to the noVNC server with your browser at:"
+    echo "Connect to the KitClient with your browser at:"
     gp url 6901
     echo ""
     echo "OR"
     echo ""
 
-    echo "Do the following on your machine to connect via VNC:"
+    echo "Do the following on your machine to connect to the KitClient via a VNC client:"
     SSH_URL=$(gp ssh | cut -f2 -d' ')
     echo "  Run the following command in a terminal:"
     echo "    ssh -L 5901:localhost:5901 $SSH_URL"
